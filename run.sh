4613ba00eafcece3a830a4d446824758f921d7d1
Bolsroll <0620007@gmail.com>
2026-04-27 22:06:17 +0900

    auto update

--- run.sh
+++ run.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# 環境変数（これ超重要）
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-
-cd /Users/dig/nogi-rss || exit 1
+cd /Users/dig/nogi-rss
 
 echo "=== START ==="
 date
 
-/usr/bin/python3 main.py
-/usr/bin/python3 make_member_xml.py
+# 実行
+python3 main.py
 
-/usr/bin/git add .
-/usr/bin/git commit -m "auto update" || echo "no changes"
-/usr/bin/git push
+# Git反映
+git add .
+git commit -m "auto update"
+git push origin main
 
 echo "=== END ==="
-date
